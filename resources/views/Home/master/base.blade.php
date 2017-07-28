cb8e5426cc9165647b7a15b663ea2cc82433ecaa
杨亚东 <1601372293@qq.com>
2017-07-28 10:25:45 +0800

    网站备案信息更新

--- resources/views/Home/master/base.blade.php
+++ resources/views/Home/master/base.blade.php
@@ -21,7 +21,18 @@
     @yield('right_content')
 </article>
 <footer>
-    <p><a target="_blank" href="http://www.miitbeian.gov.cn">豫ICP备17029407号-1</a> <a href="#">网站统计</a></p>
+    <div style="width:500px;margin:0 auto; padding:20px 0;">
+        <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=41162202000006"
+           style="display:inline-block;text-decoration:none;height:20px;line-height:20px;"><img
+                    src="/image/beian.gov.png"
+                    style="float:left;"/>
+            <p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#939393;">豫公网安备
+                41162202000006号</p></a>
+        <a target="_blank" href="http://www.miitbeian.gov.cn"
+           style="display:inline-block;text-decoration:none;height:20px;line-height:20px;"><p
+                    style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#939393;">
+                豫ICP备17029407号-1</p></a>
+    </div>
 </footer>
 </body>
 <script type="text/javascript" src="/Home/js/jquery-2.0.3.min.js"></script>
